8b22bdc5ae619d44422406443e727b34a03f95fc
PhilSanders <phil@sourcetoad.com>
2019-01-20 12:09:00 -0500

    adds audio visualizer

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,6 +21,64 @@ let libPathInput
 let audioPlayer = document.querySelector('#AudioPlayer')
 let audioSource = document.querySelector('#AudioMp3')
 
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
+//var audio = document.getElementById('audio');
+var ctx = new AudioContext();
+var analyser = ctx.createAnalyser();
+var audioSrc = ctx.createMediaElementSource(audioPlayer);
+// we have to connect the MediaElementSource with the analyser
+audioSrc.connect(analyser);
+analyser.connect(ctx.destination);
+// we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+// analyser.fftSize = 64;
+// frequencyBinCount tells you how many values you'll receive from the analyser
+var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+// we're ready to receive some data!
+var canvas = document.getElementById('canvas'),
+    cwidth = canvas.width,
+    cheight = canvas.height - 2,
+    meterWidth = 10, //width of the meters in the spectrum
+    gap = 2, //gap between meters
+    capHeight = 2,
+    capStyle = '#fff',
+    meterNum = 800 / (10 + 2), //count of the meters
+    capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+
+ctx = canvas.getContext('2d'),
+gradient = ctx.createLinearGradient(0, 0, 0, 300);
+gradient.addColorStop(1, '#8A2BE2');
+gradient.addColorStop(0.5, '#8A2BE2');
+gradient.addColorStop(0, '#ab68ea');
+
+// loop
+function renderFrame() {
+    var array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
+    var step = Math.round(array.length / meterNum); //sample limited data from the total array
+    ctx.clearRect(0, 0, cwidth, cheight);
+    for (var i = 0; i < meterNum; i++) {
+        var value = array[i * step];
+        if (capYPositionArray.length < Math.round(meterNum)) {
+            capYPositionArray.push(value);
+        };
+        ctx.fillStyle = capStyle;
+        //draw the cap, with transition effect
+        if (value < capYPositionArray[i]) {
+            ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+        } else {
+            ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+            capYPositionArray[i] = value;
+        };
+        ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
+        ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+    }
+    requestAnimationFrame(renderFrame);
+}
+// renderFrame();
+// audio.play();
+
 const consoleOut = document.querySelector("#ConsoleLog span")
 const updateConsole = (text) => {
   // consoleOut.title = text
@@ -331,6 +389,8 @@ const Library = new function() {
         audioSource.src = fileBuffer
         audioPlayer.load()
         audioPlayer.play()
+
+        renderFrame();
       })
     }
 
